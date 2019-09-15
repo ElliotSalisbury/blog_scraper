8eb50a66cc9d458131aae0f6bd5e22e60b5aaa1a
Elliot <elliot.sal@gmail.com>
2019-09-15 21:16:51 +0200

    search google for blog/news page on the domains we found in the urk redirect

--- google_search.py
+++ google_search.py
@@ -1,25 +1,49 @@
 import csv
+import os
 
 from urllib.parse import urlparse
 from url_normalize import url_normalize
 from googlesearch import search
 
-DATA_FILE = r"C:\Users\Elliot\PycharmProjects\emint\scraper\duedil\joined3.csv"
+URL_FILE = r".\urls.csv"
+OUT_FILE = r".\news.csv"
 
-with open(DATA_FILE, "r") as f:
+done = set()
+out = []
+if os.path.exists(OUT_FILE):
+    with open(OUT_FILE, "r") as f:
+        reader = csv.reader(f)
+        for i, row in enumerate(reader):
+            done.add(row[0])
+            out.append(row)
+
+with open(URL_FILE, "r") as f:
     reader = csv.reader(f)
     for i, row in enumerate(reader):
-        if i == 0:
+        if row[0] in done:
             continue
 
-        if row[7] == "High - A":
-            sfid = row[0]
-            website = row[2]
-            url_normed = url_normalize(website)
+        sfid = row[0]
+        name = row[1]
+        url = row[2]
+
+        if len(row) == 6:
+            url = row[5]
+
+        try:
+            url_normed = url_normalize(url)
             url_hostname = urlparse(url_normed).hostname.replace("www.", "")
 
             print(f"{url_hostname}")
-            query = f"site:{url_hostname} news"
-            results = search(query=query, num=10,stop=10,pause=2)
+            query = f"site:{url_hostname} news OR blog OR press"
+            results = set(search(query=query, num=10,stop=10,pause=2))
             for r in results:
+                out.append([sfid, r])
                 print(f"\t{r}")
+        except:
+            out.append([sfid,])
+
+        with open(OUT_FILE, "w", newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(out)
+
